388a203c9016f3e29b1c006cf0336fadf86ed30a
fkiarie <kazungu61@outlook.com>
2026-01-24 13:33:37 +0300

    Add search and filter reset functionality; implement basic table sorting in list.php

--- sows/list.php
+++ sows/list.php
@@ -224,7 +224,55 @@ function filterTable() {
     });
 }
 
-search.oninput = filter.onchange = filterTable;
+// Event listeners for searching and filtering
+if(search) search.oninput = filterTable;
+if(filter) filter.onchange = filterTable;
 
 function resetFilters() {
     search.value = '';
+    filter.value = '';
+    filterTable();
+}
+
+/**
+ * Basic Table Sort
+ */
+function sortTable(n) {
+    var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
+    table = document.getElementById("sowsTable");
+    switching = true;
+    dir = "asc"; 
+    while (switching) {
+        switching = false;
+        rows = table.rows;
+        for (i = 1; i < (rows.length - 1); i++) {
+            shouldSwitch = false;
+            x = rows[i].getElementsByTagName("TD")[n];
+            y = rows[i + 1].getElementsByTagName("TD")[n];
+            if (dir == "asc") {
+                if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            } else if (dir == "desc") {
+                if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+        }
+        if (shouldSwitch) {
+            rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+            switching = true;
+            switchcount ++;      
+        } else {
+            if (switchcount == 0 && dir == "asc") {
+                dir = "desc";
+                switching = true;
+            }
+        }
+    }
+}
+</script>
+
+<?php require_once __DIR__ . '/../includes/footer.php'; ?>
